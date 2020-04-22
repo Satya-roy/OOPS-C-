4de7e152b471c310834b79f7ede13b923d2370a0
Satya-roy <samantroy04@gmail.com>
2020-04-22 09:46:15 +0530

    intro to OOPS

---
+++ lect_1_Intro.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class HumanBeing{
+    public:
+    void display(){
+        cout<<"I am a human being";
+    }
+};
+
+int main(){
+    HumanBeing satya;
+    satya.display();
+    return 0;
+}
